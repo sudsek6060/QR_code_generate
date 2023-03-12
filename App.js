ea240f80123bb18c3a4cd7efff197f2842560268
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-12 21:30:48 +0530

    clean code

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
  
  const makeQrCode =  function(){
-    // e.preventDefault();
-    let text = document.querySelector('.data').value;
-    // let value = text.value
-    let qrSrc = `http://api.qrserver.com/v1/create-qr-code/?data=${text}`;
-    let qrImg = document.querySelector('#img');
+    const text = document.querySelector('.data').value;
+    const qrSrc = `http://api.qrserver.com/v1/create-qr-code/?data=${text}`;
+    const qrImg = document.querySelector('#img');
     qrImg.src = qrSrc;
      document.querySelector('.main').style.height = '600px'
  }
